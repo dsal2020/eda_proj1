dd2423165f6138b634c2ca0aab4a109fca064b5c
dsal2020 <garg.ck@gmail.com>
2015-04-07 22:23:58 -0500

    proj1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+require(lubridate)
+# reads only data for Feb 1-2 2007 and the column names 
+electric_data<-read.table("household_power_consumption.txt",sep=";",
+                          na.strings="?",skip=66637,nrows=2880, header = FALSE)
+
+colnames<-read.table("household_power_consumption.txt",sep=";",nrows=1, 
+                     header = FALSE)
+# assign column names to dataset
+colnames<-t(colnames)
+names(electric_data)<-colnames
+
+# reclass Date & Time columns
+
+electric_data$Time<-paste(electric_data$Date,electric_data$Time) # so time stamp is accurate
+electric_data$Date<-dmy(electric_data$Date) 
+electric_data$Time<-strptime(electric_data$Time,"%d/%m/%Y %H:%M:%S")
+
+hist(electric_data$Global_active_power,
+     main="Global Active Power",col="red",xlab="Global active power(kilowatts)")
+
+dev.copy(png,file="plot1.png")
+dev.off()
+
+
+
---
+++ plot4.R
@@ -0,0 +1,39 @@
+require(lubridate)
+# reads only data for Feb 1-2 2007 and the column names 
+electric_data<-read.table("household_power_consumption.txt",sep=";",
+                          na.strings="?",skip=66637,nrows=2880, header = FALSE)
+
+colnames<-read.table("household_power_consumption.txt",sep=";",nrows=1, 
+                     header = FALSE)
+# assign column names to dataset
+colnames<-t(colnames)
+names(electric_data)<-colnames
+
+# reclass Date & Time columns
+
+electric_data$Time<-paste(electric_data$Date,electric_data$Time) # so time stamp is accurate
+electric_data$Date<-dmy(electric_data$Date) 
+electric_data$Time<-strptime(electric_data$Time,"%d/%m/%Y %H:%M:%S")
+
+par(mfrow=c(2,2))
+
+plot(electric_data$Time,electric_data$Global_active_power,
+     xlab="Time",ylab="Global Active Power",type="l")
+
+plot(electric_data$Time,electric_data$Voltage,
+     xlab="datetime",ylab="Voltage",type="l")
+
+plot(electric_data$Time,electric_data$Sub_metering_1, 
+     type="l",ylab="Energy sub metering",xlab="")
+lines(electric_data$Time,electric_data$Sub_metering_2,col="red")
+lines(electric_data$Time,electric_data$Sub_metering_3,col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),cex=0.75)
+
+plot(electric_data$Time,electric_data$Global_reactive_power,
+     xlab="datetime",ylab="Global_reactive_power",type="l")
+
+dev.copy(png,file="plot4.png")
+dev.off()
+
+
+
